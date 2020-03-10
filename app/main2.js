2e110c36f2d9364bcdd79eb9a2ba2ad94582bdc5
Eric Laursen <ericjlaursen@gmail.com>
2020-03-09 20:08:15 -0400

    importing electron

---
+++ app/main2.js
@@ -0,0 +1,17 @@
+/*
+	Electron API docs
+	https://www.electronjs.org/docs/api
+*/
+
+/*
+	import electron
+	...could use 
+	const electron = require('electron')
+*/
+
+/*
+	APP docs
+	https://www.electronjs.org/docs/api/app
+	contains methods && events
+*/
+const { app } = require('electron')
