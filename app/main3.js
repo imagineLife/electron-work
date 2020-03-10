2ce58562ca206dc618c253e5e9e6c815bb60f057
Eric Laursen <ericjlaursen@gmail.com>
2020-03-09 20:21:26 -0400

    loads window after wait

--- app/main3.js
+++ app/main3.js
@@ -7,11 +7,34 @@ const { app, BrowserWindow } = require('electron')
 let eWin = null;
 app.on('ready', () => {
 
+	/*
+		V1, shows window with a white loading 'flash'
+	*/
 	// BROWSER WINDOW 
 	// https://www.electronjs.org/docs/api/browser-window#browserwindow
-	eWin = new BrowserWindow()
+	// eWin = new BrowserWindow()
 
 	// LOAD FILE
 	// https://www.electronjs.org/docs/api/browser-window#winloadfilefilepath-options
+	// eWin.loadFile(`${__dirname}/index.html`)
+
+	/*
+		V2
+		doesn't show window till loaded
+	*/
+	eWin = new BrowserWindow({ show: false })
 	eWin.loadFile(`${__dirname}/index.html`)
+
+	/*
+		NODE api doc on ONCE
+		https://nodejs.org/api/events.html#events_emitter_once_eventname_listener
+		...Adds a one-time listener 
+			function for the event named eventName. 
+			The next time eventName is triggered, 
+			this listener is removed and then invoked.
+	*/
+	eWin.once('ready-to-show', () => {
+		eWin.show()
+	})
+
 })
