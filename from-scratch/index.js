76ea36916ef1a1a8aae74f298729aa5dd95dbdcf
Eric Laursen <ericjlaursen@gmail.com>
2020-04-03 06:38:52 -0400

    removes unused file

--- from-scratch/index.js
+++
@@ -1,8 +0,0 @@
-const React = require("react")
-const ReactDOM = require("react-dom")
-require('./index.css') 
-const App = () => {
-	return(<p>react component</p>)
-}
-
-ReactDOM.render(<App />, document.getElementById("app"));
